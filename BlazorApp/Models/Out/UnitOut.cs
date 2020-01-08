d6a523717a1f76ba2b0d9afedaa1f16b611485c3
Marcin Flisiuk <artilist@tlen.pl>
2020-01-08 05:42:20 +0100

    Dodano inputy stawki vat i jednostki miary wyświetlane w zależności od tego, czy tworzymy nowy towar w widoku wprowadzania faktury zakupowej.

---
+++ BlazorApp/Models/Out/UnitOut.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models.Out
+{
+    public class UnitOut
+    {
+        public string Name { get; set; }
+    }
+}
